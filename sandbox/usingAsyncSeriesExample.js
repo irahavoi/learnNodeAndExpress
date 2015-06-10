b16405543a0041dee46a045f1a0bccf61571c76e
unknown <irahavoi@gmail.com>
2015-06-10 15:39:16 -0500

    Up

---
+++ sandbox/usingAsyncSeriesExample.js
@@ -0,0 +1,30 @@
+/**
+ * Created by irahavoi on 6/10/2015.
+ */
+var async = require('async'),
+    request = require('request');
+
+function done(err, results){
+    if(err){
+        throw err;
+    }
+    console.log("results: %j", results);
+}
+
+
+//Chaining execution of two I/O operations using the async.series helper function:
+async.series([
+    function(next){
+        request.post({uri: 'http://localhost:8080', body: '4'},
+            function(err, res, body){
+                next(err, body && JSON.parse(body));
+        })
+    },
+
+    function(next){
+        request.post({uri: 'http://localhost:8080', body: '5'},
+            function(err, res, body){
+                next(err, body && JSON.parse(body));
+            })
+    }
+], done);
