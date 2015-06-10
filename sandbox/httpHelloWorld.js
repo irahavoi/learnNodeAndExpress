3bad5c6ca4e4f688d5f197d04e59376d1a4b5ecb
unknown <irahavoi@gmail.com>
2015-06-10 10:48:48 -0500

    Up

--- sandbox/httpHelloWorld.js
+++ sandbox/httpHelloWorld.js
@@ -5,7 +5,7 @@ var http = require('http');
 var server = http.createServer();
 
 server.on('request', function(req, res){
-   console.log('Got request for the url: ' + req.url);
+   console.log('Got request for the url : ' + req.url);
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.write('Hello World!');
    res.end();
